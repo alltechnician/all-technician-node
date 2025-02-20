385449471a3b38c0f117b8e1ef4ff0fa37d0fc29
Mohammed Thasthakir <mohammed.mt392@gmail.com>
2025-02-20 20:27:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('running');
 });
 app.use('/api/admin/admin-user', adminUserRoutes);
